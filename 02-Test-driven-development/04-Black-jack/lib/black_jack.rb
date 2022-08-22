f55823a7f81a58f8cdc4391407dba5a27b3eb0e2
AntoineDrsl <antoine.durussel@gmail.com>
2022-08-22 17:15:33 +0200

    :sparkles: exercices

--- 02-Test-driven-development/04-Black-jack/lib/black_jack.rb
+++ 02-Test-driven-development/04-Black-jack/lib/black_jack.rb
@@ -1,1 +1,42 @@
 # TO DO: Write a (simplified) Black Jack game.
+
+require './dealer.rb'
+
+# Global variables
+$cards = [2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11]
+
+# Variables
+player_hand = []
+dealer_hand = []
+
+# Hits dealer cards
+dealer_hand.push(hit())
+dealer_hand.push(hit())
+puts "Dealer: #{hand_value(dealer_hand)}"
+
+# Hits player cards
+player_hand.push(hit())
+player_hand.push(hit())
+puts "Player: #{hand_value(player_hand)}"
+
+# Player choice
+while hand_value(player_hand) < 21
+  puts('Draw card ? (Yes/No)')
+  action = gets.chomp
+  if action.downcase == 'yes' || action.downcase == 'y'
+    player_hand.push(hit())
+    puts "Player: #{hand_value(player_hand)}"
+  else 
+    break
+  end
+end
+
+puts special_messages(hand_value(player_hand))
+
+# Dealer play
+dealer_hand = dealer_play(dealer_hand)
+puts "Dealer: #{hand_value(dealer_hand)}"
+puts special_messages(hand_value(dealer_hand))
+
+# Result
+puts result_message(hand_value(player_hand), hand_value(dealer_hand))
--- 02-Test-driven-development/04-Black-jack/lib/dealer.rb
+++ 02-Test-driven-development/04-Black-jack/lib/dealer.rb
@@ -3,3 +3,38 @@
 # - dealers_hand
 # - state_of_game
 # - end_of_game
+
+# Pick card from cards
+def hit()
+  index = rand($cards.length)
+  card = $cards[index]
+  $cards.delete_at(index)
+  return card
+end
+
+# Dealer turn
+def dealer_play(dealer_hand)
+  total = hand_value(dealer_hand)
+  while total < 17
+    dealer_hand.push(hit())
+  end
+  return dealer_hand
+end
+
+# Get hand score
+def hand_value(hand)
+  return hand.sum
+end
+
+# Print special messages
+def special_messages(value)
+  return 'Blackjack !' if value == 21
+  return 'Bust !' if value > 21
+end
+
+# Return game result
+def result_message(player_score, dealer_score)
+  return 'Equility'  if player_score == dealer_score || (player_score > 21 && dealer_score > 21)
+  return 'You win !' if player_score > dealer_score && player_score < 21
+  return 'You loose !' if player_score < dealer_score && dealer_score < 21
+end
