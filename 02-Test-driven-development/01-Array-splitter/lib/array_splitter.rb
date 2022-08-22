17ceb7872f6993c111861fd4ab1da8882225e443
AntoineDrsl <antoine.durussel@gmail.com>
2022-08-22 15:02:54 +0200

    :sparkles: tdd array splitter exercice

--- 02-Test-driven-development/01-Array-splitter/lib/array_splitter.rb
+++ 02-Test-driven-development/01-Array-splitter/lib/array_splitter.rb
@@ -1,1 +1,10 @@
 # TO DO: Write a method that splits an array into 2 different ones.
+
+def array_splitter(array, index)
+  return 'First argument must be an array' unless array.is_a? Array
+  return 'Second argument must be an integer' unless index.is_a? Integer
+  return 'Index must must not be negative' unless index >= 0
+  return 'Index is not in array' if array[index].nil?
+
+  return [array[..index], array[(index + 1)..]]
+end
--- 02-Test-driven-development/01-Array-splitter/spec/array_splitter_spec.rb
+++ 02-Test-driven-development/01-Array-splitter/spec/array_splitter_spec.rb
@@ -1,2 +1,41 @@
 # Remember: we are doing TDD here, so don’t rush in the program.
 # You need to write the tests first.
+
+require 'array_splitter'
+
+describe '#array_splitter' do
+  it 'return an error if first argument is not an array.' do
+    error = 'First argument must be an array'
+
+    expect(array_splitter('hi', 3)).to eq(error)
+    expect(array_splitter(3, 3)).to eq(error)
+    expect(array_splitter(true, 3)).to eq(error)
+  end
+
+  it 'return an error if second argument is not an integer.' do
+    error = 'Second argument must be an integer'
+
+    expect(array_splitter([1, 2, 3], 'hi')).to eq(error)
+    expect(array_splitter([1, 2, 3], [1, 2])).to eq(error)
+    expect(array_splitter([1, 2, 3], 1.1)).to eq(error)
+  end
+
+  it 'return an error if index is negative.' do
+    error = 'Index must must not be negative'
+
+    expect(array_splitter([1, 2, 3], -5)).to eq(error)
+  end
+
+  it 'return an error if index is not in array.' do
+    error = 'Index is not in array'
+
+    expect(array_splitter([1, 2, 3], 3)).to eq(error)
+    expect(array_splitter([1, 2, 3], 54)).to eq(error)
+  end
+
+  it 'splits an array at a given index and return two arrays.' do
+    expect(array_splitter([1, 2, 3], 1)).to eq([[1, 2], [3]])
+    expect(array_splitter([1, 2, 3], 0)).to eq([[1], [2, 3]])
+    expect(array_splitter([1, 2, 3], 2)).to eq([[1, 2, 3], []])
+  end
+end
