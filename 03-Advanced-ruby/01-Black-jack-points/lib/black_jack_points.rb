615ab28e515193009f4d6ea5b4a94af7ab18c0f0
AntoineDrsl <antoine.durussel@gmail.com>
2022-08-23 11:08:12 +0200

    :sparkles: IMPROVE blackjack

--- 03-Advanced-ruby/01-Black-jack-points/lib/black_jack_points.rb
+++ 03-Advanced-ruby/01-Black-jack-points/lib/black_jack_points.rb
@@ -1,1 +1,44 @@
-# TO DO: Write a method that maps the different Black Jack cards to their corresponding score.
+# TO DO: Write a (simplified) Black Jack game.
+require_relative 'dealer'
+
+# Global variables
+$cards = ['Ace', 'Ace', 'Ace', 'Ace', '2', '2', '2', '2', '3', '3', '3', '3', '4', '4', '4', '4', '5', '5', '5', '5', '6', '6', '6', '6', '7', '7', '7', '7', '8', '8', '8', '8', '9', '9', '9', '9', '10', '10', '10', '10', 'Jack', 'Jack', 'Jack', 'Jack', 'Queen', 'Queen', 'Queen', 'Queen', 'King', 'King', 'King', 'King']
+$turn = 'dealer'
+
+# Variables
+player_hand = []
+dealer_hand = []
+
+# Hits dealer cards
+dealer_hand.push(hit())
+dealer_hand.push(hit())
+puts "Dealer: #{hand_value(dealer_hand)}"
+
+# Hits player cards
+$turn = 'player'
+player_hand.push(hit())
+player_hand.push(hit())
+puts "Player: #{hand_value(player_hand)}"
+
+# Player choice
+while hand_value(player_hand) < 21
+  puts('Draw card ? (Yes/No)')
+  action = STDIN.gets.chomp
+  if action.downcase == 'yes' || action.downcase == 'y'
+    player_hand.push(hit())
+    puts "Player: #{hand_value(player_hand)}"
+  else 
+    break
+  end
+end
+
+puts special_messages(hand_value(player_hand))
+
+# Dealer play
+$turn = 'dealer'
+dealer_hand = dealer_play(dealer_hand)
+puts "Dealer: #{hand_value(dealer_hand)}"
+puts special_messages(hand_value(dealer_hand))
+
+# Result
+puts result_message(hand_value(player_hand), hand_value(dealer_hand))
---
+++ 03-Advanced-ruby/01-Black-jack-points/lib/dealer.rb
@@ -0,0 +1,78 @@
+# TO DO: Write in this file the following methods
+# - hit
+# - dealers_hand
+# - state_of_game
+# - end_of_game
+
+def card_score(card)
+  scores = {
+    'Ace': 11,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'Jack': 10,
+    'Queen': 10,
+    'King': 10
+  }
+
+  if card == 'Ace' && $turn == 'player'
+    puts 'Ace ! What score do you want ? (1/11)'
+    while true
+      choice = STDIN.gets.chomp
+      if choice == '1' || choice == '11'
+        puts choice
+        score = choice.to_i
+        break
+      else
+        puts 'An Ace can be A or 11. What do you want ? (1/11)'
+      end
+    end
+  else
+    score = scores[card.to_sym]
+  end
+
+  puts card
+
+  return score
+end
+
+# Pick card from cards
+def hit()
+  index = rand($cards.length)
+  card = $cards[index]
+  score = card_score(card)
+  $cards.delete_at(index)
+  return score
+end
+
+# Dealer turn
+def dealer_play(dealer_hand)
+  while hand_value(dealer_hand) < 17
+    dealer_hand.push(hit())
+  end
+  return dealer_hand
+end
+
+# Get hand score
+def hand_value(hand)
+  return hand.sum
+end
+
+# Print special messages
+def special_messages(score)
+  return 'Blackjack !' if score == 21
+  return 'Bust !' if score > 21
+end
+
+# Return game result
+def result_message(player_score, dealer_score)
+  return 'Equility'  if player_score == dealer_score || (player_score > 21 && dealer_score > 21)
+  return 'You win !' if (player_score > dealer_score && player_score <= 21) || dealer_score > 21
+  return 'You loose !' if (player_score < dealer_score && dealer_score <= 21) || player_score > 21
+end
